729297edf2db51c08997a5ad18ebf57f49407ea1
Fae <109899113+iamf-a-e@users.noreply.github.com>
2025-05-30 15:04:23 +0200

    products.py

--- products.py
+++ products.py
@@ -3,8 +3,11 @@ class Product:
         self.name = name
         self.price = price
         self.description = description
-        self.products = []
 
+class Category:
+    def __init__(self, name):
+        self.name = name
+        self.products = []
 
     def add_product(self, product):
         self.products.append(product)
