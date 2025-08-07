7a7477011da7382c5176ae956bc4a2e3e59712b1
CMunzwa <cmunzwa@gmail.com>
2025-08-07 12:38:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ phone_id = os.environ.get("PHONE_ID")
 gen_api = os.environ.get("GEN_API")
 owner_phone = os.environ.get("OWNER_PHONE")
 redis_url = os.environ.get("REDIS_URL")
-ADMIN_NUMBERS = ["263719835124", "263785913291", "263777113588", "263785019494"]
+ADMIN_NUMBERS = ["263719835124", "263785913291", "263777113588"]
 AGENT_NUMBERS = ["263777113588"]
 
 
